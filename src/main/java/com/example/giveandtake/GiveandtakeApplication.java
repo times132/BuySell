bba96528d1d26886e1293c4a1fa54d810286aace
times132 <times132@gmail.com>
2020-02-09 15:51:40 +0900

    base

---
+++ src/main/java/com/example/giveandtake/GiveandtakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.giveandtake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveandtakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveandtakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/giveandtake/GiveandtakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giveandtake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveandtakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
